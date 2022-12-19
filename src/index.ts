146553b9095e126c6fdb56649f57ebc8b1c2491c
balaxon <balahanov2209@gmail.com>
2022-12-19 22:33:23 +0300

    init: start online-store task

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import './style.scss';
+console.log('test');
+const asd = 'qwe';
+console.log(asd);
+
+const img = document.createElement('img');
+img.src = 'assets/images/bird.jpg';
+const body = document.getElementById('body');
+document.getElementById('body')?.append(img);
+
+const tt: number = 0;
+
+console.log(tt);
+
+const new1 = {
+    sd: 'asd',
+};
+console.log(new1);
