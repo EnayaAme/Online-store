3783d5fd53b1f132ee0aed234f2e5537d362c423
EnayaAme <enaya13ame@gmail.com>
2022-12-26 15:31:35 +0100

    fear: add start page base

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import './style.scss';
 import data from './data';
+import CreateDefaultPage from './ts/start';
 
 const products = new data().get();
 console.log(products);
+
+const Page = new CreateDefaultPage();
+Page.CreateHeader();
+Page.CreateMain();
+//Page.CreateFooter();
